5143e486fb283eb441f22368bdaf3ec6cf126377
gitafadia <e41200971@polije.ac.id>
2022-04-23 20:02:45 +0700

    tugas_minggu9

---
+++ lib/model/product.dart
@@ -0,0 +1,9 @@
+class Product {
+  final int id;
+  final String productName;
+  final String productImage;
+  final String productDescription;
+  final double price;
+
+  Product(this.id, this.productName, this.productDescription, this.productImage, this.price);
+}
